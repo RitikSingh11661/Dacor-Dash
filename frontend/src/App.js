ea7874de01656c184e7367682fe37346615892d9
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-03-29 23:20:28 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import { Navbar } from './components/Navbar';
 import { Allroutes } from './routes/Allroutes';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Allroutes/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Allroutes />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
