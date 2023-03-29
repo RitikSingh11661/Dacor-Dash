0ebaf8831a30cbc79c25fb3316b37e87f162deaa
Ritik Singh <delhi8260@gmail.com>
2023-03-29 23:12:47 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+import { Allroutes } from './routes/Allroutes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Allroutes/>
+      <Footer/>
     </div>
   );
 }
