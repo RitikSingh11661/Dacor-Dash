6c04fd09829f0d1a7c3f2ac648ac677b34932a12
Ritik Singh <delhi8260@gmail.com>
2023-03-30 12:35:21 +0530

    authentication pages done

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,15 +1,15 @@
-import { Flex, Box, FormControl, FormLabel, Input, Stack, Button, Heading, Text, useToast } from "@chakra-ui/react";
+import { Flex, Box, FormControl, FormLabel, Input, Stack, Button, Heading, Text, useToast} from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { useGoogleOneTapLogin } from '@react-oauth/google';
-// import { GoogleLogin } from "@react-oauth/google";
-import {GoogleLoginButton} from 'react-social-login-buttons'
+import { GoogleLoginButton } from "react-social-login-buttons";
 import { getUsers, signup } from "../redux/Auth/actions";
-import { useGoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin, useGoogleOneTapLogin } from "@react-oauth/google";
+import axios from "axios";
+// import Image from '../../assets/signupbg.jpg'
 
 export const Signup = () => {
-  const initUser = { email: "", password: "", name: "", cart: [], orders: [] }
+  const initUser = { email: "", password: "", name: "", cart: [], orders: [] };
   const [user, setUser] = useState(initUser);
   const toast = useToast();
   const dispatch = useDispatch();
@@ -18,13 +18,25 @@ export const Signup = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUser({ ...user, [name]: value })
-  }
+    setUser({ ...user, [name]: value });
+  };
+
+  const getUserDetails = (tokenResponse) => {
+    try {
+      const accessToken = tokenResponse.access_token;
+      fetch("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    } catch (error) {
+      console.log("error while login", error);
+    }
+  };
 
   const login = useGoogleLogin({
-    onSuccess: tokenResponse => console.log(tokenResponse),
-    scope:['https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/userinfo.profile']
-  })
+    onSuccess: (tokenResponse) => getUserDetails(tokenResponse),
+  });
 
   // normal with api
   const handleSubmit = (e) => {
@@ -45,7 +57,7 @@ export const Signup = () => {
     } else {
       dispatch(signup(user, newToastSucess, newToastFail)).then(() => {
         dispatch(getUsers);
-        navigate('/login')
+        navigate("/login");
       });
     }
   };
@@ -74,30 +86,47 @@ export const Signup = () => {
     });
   };
 
-  // ..................//
-  // Google Authentication
+  // Google One Tap Authentication
 
   // useGoogleOneTapLogin({
-  //   onSuccess: credentialResponse => {
-  //     console.log(credentialResponse);
+  //   onSuccess: credentialResponse=>{
+  //     if (credentialResponse.credential) {
+  //       const idToken = credentialResponse.credential;
+  //       const url =`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${idToken}`
+  //       axios.get(url).then(res=>console.log('res',res));
+  //     }
   //   },
   //   onError: () => {
   //     console.log('Login Failed');
   //   },
   // });
-
-
+  
   return (
-    <Flex pt={"-200px"} minH={"100vh"} align={"center"} justify={"center"} m="auto">
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      backgroundImage={"url('https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')"}
+      backgroundSize={"cover"}
+      backgroundPosition={"center"}
+    >
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
-          <Heading color={"#002E6E"} fontSize={"4xl"}>Create your account</Heading>
+          <Heading color={"white"} fontSize={"4xl"}>
+            Create your account
+          </Heading>
         </Stack>
-        <Box rounded={"lg"} boxShadow={"lg"} p={8} >
+        <Box
+          rounded={"lg"}
+          boxShadow={"lg"}
+          p={8}
+          bg={"white"}
+          background={"rgba(255, 255, 255, 1.75)"}
+        >
           <form onSubmit={handleSubmit}>
-            <Stack spacing={2}>
+            <Stack spacing={4}>
               <FormControl id="name">
-                <FormLabel>Name</FormLabel>
+                <FormLabel color={"#002E6E"}>Name</FormLabel>
                 <Input
                   focusBorderColor="#002E6E"
                   borderColor={"#002E6E"}
@@ -110,7 +139,7 @@ export const Signup = () => {
                 />
               </FormControl>
               <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
+                <FormLabel color={"#002E6E"}>Email address</FormLabel>
                 <Input
                   focusBorderColor={"#002E6E"}
                   placeholder="Enter @gmail.com"
@@ -123,7 +152,7 @@ export const Signup = () => {
                 />
               </FormControl>
               <FormControl id="password">
-                <FormLabel>Password</FormLabel>
+                <FormLabel color={"#002E6E"}>Password</FormLabel>
                 <Input
                   focusBorderColor={"#002E6E"}
                   placeholder="Enter password ***"
@@ -135,29 +164,29 @@ export const Signup = () => {
                   required
                 />
               </FormControl>
-              <Stack spacing={4}>
-                <Button
-                  type="submit"
-                  bg={"blue.500"}
-                  color={"white"}
-                  _hover={{
-                    bg: "blue.600",
-                  }}
-                >
-                  Sign in
-                </Button>
-                <GoogleLoginButton onClick={login}></GoogleLoginButton>
-                <Box display={"flex"} justifyContent="center">
-                  <Text as={"span"} textAlign={"center"}>Already have Account ?{" "}</Text>
-                  <Text color="#002E6E" fontWeight="600" as={"span"}>
-                    <Link to={"/login"}>Login</Link>
-                  </Text>
-                </Box>
-              </Stack>
+              <Button
+                type="submit"
+                bg={"blue.500"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.600",
+                }}
+              >
+                Sign up
+              </Button>
+              <GoogleLoginButton onClick={login}></GoogleLoginButton>
+              <Box display={"flex"} justifyContent="space-evenly">
+                <Text as={"span"}  textAlign={"center"}>
+                  Already have an account?{" "}
+                </Text>
+                <Text fontWeight="600" color="#002E6E" as={"span"}>
+                  <Link to={"/login"}>Log in</Link>
+                </Text>
+              </Box>
             </Stack>
           </form>
         </Box>
       </Stack>
     </Flex>
-  );
-}
+  );  
+};
